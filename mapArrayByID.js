44287aad349f37119c7e02a8f6165751ec6a7d62
phat24112004 <phath9548@gmail.com>
2025-09-17 14:41:16 +0700

    Update mapArrayByID.js

--- mapArrayByID.js
+++ mapArrayByID.js
@@ -2,8 +2,8 @@ const users = [
     {id: 1, name: 'Alice'},
     {id: 2, name: 'Bob'}
 ];
-const result = users.reduce((acc, user) => {
-    acc[user.id] = user.name;
-    return acc;
+const result = users.reduce((value, index) => {
+    value[index.id] = index.name;
+    return value;
 }, {});
 console.log(result);
